2d67716806ab286f07226a7f8197904e51eaca51
Dzuchun <Infernodambik@gmail.com>
2024-04-10 21:22:34 +0300

    Implemented task_4_10

--- task_4_rest.c
+++ task_4_rest.c
@@ -2,11 +2,9 @@
 // calculator)
 //
 // Hope to implement at least a couple
-//
-// NOTE: this code intentionally avoids using std library. the point is to get
-// accustomed to the language, not fast developing
 
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 
 #define INPBUF_SIZE 100
@@ -14,54 +12,51 @@
  * This is an input char buffer, used to recognize and parse input.
  */
 char inpbuf[INPBUF_SIZE];
-int inpe = 0; // points past the end of the buffer (inpbuf[inpe] is illegal)
+int inpe = 0;
+int inpb = 0; // points to next unparsed char in the input buffer
+
+int input_len() { return (inpe >= inpb) ? inpe - inpb : 0; }
+
+void input_clear() { inpb = inpe = 0; }
+
+void input_fetch() {
+    int i;
+    char c;
+    if (inpb < inpe)
+        // there are still characters in the buffer
+        return;
+
+    inpb = inpe = 0;
+
+    while (!feof(stdin) && !input_len()) {
+
+        fgets(&inpbuf[0], INPBUF_SIZE, stdin);
+
+        for (i = 0;
+             (i < INPBUF_SIZE) && ((c = inpbuf[i]) != '\0') && (c != '\n'); ++i)
+            ;
+        // we either reached the end (kinda bad!)
+        // or encountered a NULL-byte or newline
+        // replace newline with NULL
+        if (c == '\n')
+            inpbuf[i] = '\0';
+        inpe = i;
+    }
+    if (feof(stdin))
+        inpb = inpe = 0;
+}
 
 int input_get(int position) {
-    int i, c;
-
-    if (position < 0 || position >= INPBUF_SIZE)
-        // that's a bad request
-        return -1;
-
-    // buffer is in a normal state
-    if (position < inpe)
-        // we already have a char, return it right away
-        return inpbuf[position];
-
-    // char must be fetched
-    // fetch forward until desired char
-    for (i = inpe; i <= position && (c = getchar()) != EOF; ++i)
-        inpbuf[i] = c;
-    inpe = i;
-    return c; // this will propagate EOF too!
+    return (position < inpb || position >= inpe) ? -1 : inpbuf[position];
 }
-
-void input_forget(int position) {
-    int i;
-
-    if (position <= 0 || position >= INPBUF_SIZE)
-        // bad input; nothing to do
-        return;
-
-    if (position >= inpe) {
-        inpe = 0;
-        return;
-    } else {
-        for (i = 0; i <= (inpe - 1 - position); ++i)
-            inpbuf[i] = inpbuf[i + position];
-        inpe -= position;
-    }
-}
-
-/* clears the input buffer */
-void input_clear() { inpe = 0; }
 
 #define MAX_OPERANDS 100
 /* This is an operand stack */
 double oprds[MAX_OPERANDS];
 int oprptr = 0;
 
-/* gets an operand from stack, or warns are returns 42.0, if none is available
+/* gets an operand from stack, or warns are returns 42.0, if none is
+ * available
  */
 double pop_oprd() {
     if (oprptr <= 0) {
@@ -91,6 +86,9 @@ enum InputType {
     Dup,     // duplicate top element
     Swp,     // swap 2 top elements
     Clr,     // clears the stack
+    Sin,     // stdlib sin
+    Exp,     // stdlib exp
+    Pow,     // stdlib pow
     Eof,     // THE end
     Unknown, // couldn't recognize the input :(
 };
@@ -104,7 +102,7 @@ double parse_double(int i) {
     }
     if (c != '.') {
         // forget input up to this point
-        input_forget(i);
+        inpb = i;
         // return the result
         return mantice;
     }
@@ -116,7 +114,7 @@ double parse_double(int i) {
         c = input_get(++i);
     }
     // forget pointer up until this point
-    input_forget(i);
+    inpb = i;
     // make decimal places
     while (decimals--)
         mantice /= 10;
@@ -125,16 +123,16 @@ double parse_double(int i) {
 }
 
 int match_word(int i, const char *word) {
-    int c, t, j;
-    for (j = 0; (c = input_get(i)) != EOF && c == (t = word[j]) && t != '\0';
-         ++i, ++j)
+    int c, t = word[0], j;
+    for (j = 0; (c = input_get(i)) != -1 && c == t && t != '\0';
+         ++i, ++j, t = word[j])
         ;
     if (t != '\0') {
         // match had failed.
         return 0;
     }
     // forget the input until word's last char
-    input_forget(i);
+    inpb = i;
     return 1;
 }
 
@@ -144,33 +142,44 @@ int match_word(int i, const char *word) {
  * If it happens to be a number, it's pushed to the operands stack
  */
 enum InputType read_next() {
-    int i = 0;
-    int c = input_get(i);
+    int i;
+    int c;
     double sign = 1.0;
 
-    // skip the whitespace
-    while (isblank(c) || c == '\n')
-        c = input_get(++i);
-
-    if (c == EOF)
-        // well, it's the end
+fetch:
+    // first, ask to fetch the input (if necessary)
+    input_fetch();
+    if (!input_len())
+        // input has len 0
+        // that actually means that EOF was reached
         return Eof;
+    i = inpb;
+
+    // skip the whitespace
+
+    while (isblank((c = input_get(i))))
+        ++i;
+
+    if (c == -1) {
+        input_clear();
+        goto fetch;
+    }
 
     if (c == '*') {
         // that's a star operator!
-        input_forget(i + 1);
+        inpb = i + 1;
         return Star;
     }
 
     if (c == '/') {
         // that's a slash operator!
-        input_forget(i + 1);
+        inpb = i + 1;
         return Slash;
     }
 
     if (c == '%') {
         // that's a percent operator!
-        input_forget(i + 1);
+        inpb = i + 1;
         return Percent;
     }
 
@@ -191,19 +200,28 @@ enum InputType read_next() {
     if (match_word(i, "clr")) {
         return Clr;
     }
+    if (match_word(i, "sin")) {
+        return Sin;
+    }
+    if (match_word(i, "exp")) {
+        return Exp;
+    }
+    if (match_word(i, "pow")) {
+        return Pow;
+    }
 
     if (c == '+') {
         c = input_get(++i);
         if (!isdigit(c)) {
             // A plus followed by a non-digit => it's a plus operator!
-            input_forget(i);
+            inpb = i;
             return Plus;
         }
     } else if (c == '-') {
         c = input_get(++i);
         if (!isdigit(c)) {
             // A minus followed by a non-digit => it's a minus operator!
-            input_forget(i);
+            inpb = i;
             return Minus;
         }
 
@@ -211,7 +229,7 @@ enum InputType read_next() {
         sign = -1.0;
     } else if (!isdigit(c)) {
         // it's not a number, and not one of the operators
-        // meaning, it's unknwon input
+        // meaning, it's unknown input
         input_clear();
         return Unknown;
     }
@@ -223,32 +241,10 @@ enum InputType read_next() {
 
 int main() {
     enum InputType type;
-    double op1, op2;
-    while (1) {
+    double op1, op2, sin(double), exp(double), pow(double, double);
+
+    for (;;) {
         type = read_next();
-        // This was used for debug
-        /*
-        switch (type) {
-        case Operand:
-            printf("Operand: %f\n", pop_oprd());
-            break;
-        case Plus:
-            printf("Plus\n");
-            break;
-        case Minus:
-            printf("Minus\n");
-            break;
-        case Star:
-            printf("Star\n");
-            break;
-        case Slash:
-            printf("Slash\n");
-            break;
-        case Unknown:
-            printf("Unknown input received\n");
-            break;
-        }
-        */
         switch (type) {
         case Eof:
             // break out of the cycle
@@ -291,7 +287,7 @@ int main() {
             op2 = pop_oprd();
             if (op2 == 0.0) {
                 fprintf(stderr, "\tCan't take modulo by 0\n");
-                return -1;
+                return -2;
             }
             op1 = pop_oprd();
             put_oprd((double)((int)op1 % (int)op2));
@@ -333,12 +329,34 @@ int main() {
             else
                 put_oprd(oprds[oprptr - 1]);
             break;
+        case Sin:
+            put_oprd(sin(pop_oprd()));
+            break;
+        case Exp:
+            put_oprd(exp(pop_oprd()));
+            break;
+        case Pow:
+            op2 = pop_oprd();
+            op1 = pop_oprd();
+            if (op1 < 0.0) {
+                fprintf(stderr, "\tPower with negative base is not "
+                                "well-defined methmtically\n");
+                return -3;
+            }
+            if (op1 == 0.0 && op2 <= 0.0) {
+                fprintf(
+                    stderr,
+                    "\tPower of 0 with non-positive eponent is not defined\n");
+                return -4;
+            } else
+                put_oprd(pow(op1, op2));
+            break;
         case Clr:
             oprptr = 0;
             break;
         case Unknown:
-            // print to stderr
-            fprintf(stderr, "\tGot unknown input!\n");
+            // notify
+            fprintf(stderr, "\tThat was not a correct input\n");
             break;
         }
     }
