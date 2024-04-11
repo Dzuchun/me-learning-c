68bcc85e748a48ce875ceee8203e031e92f60ec7
Dzuchun <Infernodambik@gmail.com>
2024-04-11 18:27:25 +0300

    Changed 4-1 so that it uses pointers now, and also accepts pattern as a command arg

--- task_4_1.c
+++ task_4_1.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
-#define BUF_LEN 100
 
-int strrindex(char s[], char t[]) {
-    int last_find = -1, i, j;
-    // for each possible position in s
-    for (i = 0; i < BUF_LEN && s[i] != '\0'; ++i) {
-        // try iterating through s and t
-        for (j = 0; i + j < BUF_LEN && s[i + j] == t[j]; ++j)
+int last_str_index(char *hay, char *pat) {
+    register int s = 0;
+    char register hc, pc, *shay, *spat;
+    // first, find the end
+    while (hay[s])
+        ++s;
+    // now s is a pointer to NULL byte at the end
+    // next, iterate back
+    while (*(shay = &hay[--s])) {
+        // while trying to match a pattern
+        spat = pat;
+        while ((hc = *shay++) && hc == (pc = *spat++))
             ;
-        // if iteration has stopped at t[j] == '\0', i is a match
-        if (t[j] == '\0')
-            last_find = i;
+        // if at a break pattern was fully consumed -- that's a match!
+        if (!pc)
+            return s;
     }
-
-    // returns -1, for no index was found
-    return last_find;
+    // returns -1, it no index was found
+    return -1;
 }
 
-int main() {
-    char s[BUF_LEN], t[BUF_LEN];
-    printf("Input pattern: ");
-    fgets(&t[0], BUF_LEN, stdin);
-    while (!feof(stdin)) {
-        fgets(&s[0], BUF_LEN, stdin);
-        printf("%d\n", strrindex(&s[0], &t[0]));
+#define BUF_LEN 100
+int main(int argc, char **argv) {
+    char s[BUF_LEN], *t;
+    int ind;
+    if (argc < 1)
+        ;
+    else if (argc < 2)
+        printf("Should supply at least one argument to %s", argv[0]);
+    else {
+        t = argv[1];
+        while (!feof(stdin)) {
+            fgets(s, BUF_LEN, stdin);
+            ind = last_str_index(s, t);
+            if (ind < 0)
+                printf("No match\n");
+            else
+                printf("Matched part: %s\n", &s[ind]);
+        }
     }
 }
