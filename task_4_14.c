745d415d9e96cbc4921a5d82597eda3f91c21a0c
Dzuchun <Infernodambik@gmail.com>
2024-04-11 06:57:44 +0300

    Done 4-14

---
+++ task_4_14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define swap(t, x, y) { t tmp = x; x = y; y = tmp; } 
+
+int main() {
+    int x = 56;
+    int y = 23;
+    printf("x = %d, y = %d\n", x, y);
+
+    swap(int, x, y);
+    printf("x = %d, y = %d\n", x, y);
+}
