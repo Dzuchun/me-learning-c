7a5370351767ff0ac9f91e6410be9f7161ed0604
Dzuchun <Infernodambik@gmail.com>
2024-04-12 15:00:30 +0300

    Refactored task_4 to use pointers

--- task_4/calc.h
+++ task_4/calc.h
@@ -1,53 +1,20 @@
-/*
- * Returns length of the input buffer that is yet to be parsed
- *
- * If this function returns 0 after [input_fetch] call, it means that input file
- * had hit EOF
- */
-int input_len();
-/*
- * Clears the input buffer, discarding any unmatched chars
- */
-void input_clear();
-/*
- * Fetches next line from input, if necessary
- */
-void input_fetch();
-/*
- * Returns input buffer at position
- *
- * If supplied position is invalid, -1 is returned instead
- */
-int input_get(int i);
-/* Returns first valid begin index */
-int input_begin();
-
-/*
- * Forgets input buffer up to specified position
- *
- * If position is less than current begin position, nothing is done
- *
- * If position in greter than the end position, it's set to the end position
- */
-void forget_to(int new_bp);
-
 /* Type of the parsed input */
 enum InputType {
-    Operand, // a number, written to the oprd stack already
-    Plus,    // '+'
-    Minus,   // '-'
-    Star,    // '*'
-    Slash,   // '/'
-    Percent, // '%'
-    Top,     // print 2 top elements
-    Dup,     // duplicate top element
-    Swp,     // swap 2 top elements
-    Clr,     // clears the stack
-    Sin,     // sin library function
-    Exp,     // exp library function
-    Pow,     // pow library function
-    Eof,     // THE end
-    Unknown, // couldn't recognize the input :(
+    Operand,    // a number, written to the oprd stack already
+    Plus,       // '+'
+    Minus,      // '-'
+    Star,       // '*'
+    Slash,      // '/'
+    Percent,    // '%'
+    Top,        // print 2 top elements
+    Dup,        // duplicate top element
+    Swp,        // swap 2 top elements
+    Clr,        // clears the stack
+    Sin,        // sin library function
+    Exp,        // exp library function
+    Pow,        // pow library function
+    Incomplete, // Current line has no more inputs
+    Unknown,    // couldn't recognize the input :(
 };
 
 /*
--- task_4/main.c
+++ task_4/main.c
@@ -7,302 +7,254 @@
 #include <ctype.h>
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 
-double parse_double(int i) {
-    int decimals = 0, c = input_get(i);
-    double mantice = 0.0;
-    while (isdigit(c)) {
-        mantice = mantice * 10 + (c - '0');
-        c = input_get(++i);
+// Matches a double literal, returning pointer past it's end in the line
+//
+// If double had failed to be matched, reutrns NULL
+char *match_double(char *line, double *out) {
+    int decimals = 0, sign = 1.0;
+    char c = *line;
+    // if that's the end -- well, there's not a double here
+    if (c == '\0')
+        return NULL;
+    // first, try parsing leading + or -
+    c = *line;
+    if (c == '+')
+        // it's ok, there's nothing to do, just move the pointer
+        c = *++line;
+    else if (c == '-') {
+        // it's ok, set the sign and move the pointer
+        sign = -1.0;
+        c = *++line;
     }
+    // if c is not a digit now, it's a garbage, return NULL
+    if (!isdigit(c))
+        return NULL;
+    // iterate through the digits and update the out variable
+    *out = 0.0;
+    while (isdigit(c)) {
+        *out = *out * 10 + (c - '0');
+        c = *++line;
+    }
+    // at this point, c is NOT a digit, and line points to it
     if (c != '.') {
-        // forget input up to this point
-        forget_to(i);
-        // return the result
-        return mantice;
+        // k, we're done here
+        // return pointer to THIS unexpected char
+        return line;
     }
-    // decimal pointer was encountered
-    c = input_get(++i);
+    // decimal point was encountered
+    // step past it
+    c = *++line;
+    // iterate through decimal part
     while (isdigit(c)) {
-        mantice = mantice * 10 + (c - '0');
+        *out = *out * 10 + (c - '0');
         decimals += 1;
-        c = input_get(++i);
+        c = *++line;
     }
-    // forget pointer up until this point
-    forget_to(i);
-    // make decimal places
+    // at this point, c is not a digit and line points to it
+    // line will be the result. but first, let's properly communicate fractional
+    // part:
     while (decimals--)
-        mantice /= 10;
+        *out /= 10;
     // return the result
-    return mantice;
+    return line;
 }
 
-int match_word(int i, const char *word) {
-    int c, t = word[0], j;
-    for (j = 0; (c = input_get(i)) != -1 && c == t && t != '\0';
-         ++i, ++j, t = word[j])
-        ;
-    if (t != '\0') {
-        // match had failed.
-        return 0;
+#define MATCH_CH(line, c, r)                                                   \
+    if (**line == c) {                                                         \
+        ++*line;                                                               \
+        return r;                                                              \
     }
-    // forget the input until word's last char
-    forget_to(i);
 
-    return 1;
-}
+#define MATCH_STR(line, pat, r)                                                \
+    {                                                                          \
+        char *tmp_line = *line, c, t, *tmp_pat = pat;                          \
+        while ((c = *tmp_line++) == (t = *tmp_pat++) && t != '\0')             \
+            ;                                                                  \
+        if (t == '\0') {                                                       \
+            *line = tmp_line - 1;                                              \
+            return r;                                                          \
+        }                                                                      \
+    }
 
 /*
  * Reads next input argument or an operation.
  *
  * If it happens to be a number, it's pushed to the operands stack
  */
-enum InputType read_next() {
-    int i;
-    int c;
-    double sign = 1.0;
-
-fetch:
-    // first, ask to fetch the input (if necessary)
-    input_fetch();
-    if (!input_len())
-        // input has len 0
-        // that actually means that EOF was reached
-        return Eof;
-    i = input_begin();
-
+enum InputType read_next(char **line) {
+    char *tmp, c;
     // skip the whitespace
 
-    while (isblank((c = input_get(i))))
-        ++i;
+    while (isblank(c = **line) && c != '\0')
+        ++*line;
 
-    if (c == -1) {
-        input_clear();
-        goto fetch;
+    if (c == '\0' || c == '\n')
+        // we've hit the end, ask for more data
+        return Incomplete;
+    // at this point, c is a fist non-whitespace char
+
+    // try matching double
+    double d;
+    if ((tmp = match_double(*line, &d)) != NULL) {
+        // that's match
+        // move pointer past the number
+        *line = tmp;
+        // insert numper as operand
+        oprd_put(d);
+        // return that we've parsed an operand
+        return Operand;
     }
 
-    if (c == '*') {
-        // that's a star operator!
-        forget_to(i + 1);
-        return Star;
-    }
+    // try match single-character commands
+    MATCH_CH(line, '*', Star);
+    MATCH_CH(line, '/', Slash);
+    MATCH_CH(line, '%', Percent);
+    MATCH_CH(line, '+', Plus);
+    MATCH_CH(line, '-', Minus);
 
-    if (c == '/') {
-        // that's a slash operator!
-        forget_to(i + 1);
-        return Slash;
-    }
+    MATCH_STR(line, "top", Top);
+    MATCH_STR(line, "dup", Dup);
+    MATCH_STR(line, "swp", Swp);
+    MATCH_STR(line, "clr", Clr);
+    MATCH_STR(line, "sin", Sin);
+    MATCH_STR(line, "exp", Exp);
+    MATCH_STR(line, "pow", Pow);
 
-    if (c == '%') {
-        // that's a percent operator!
-        forget_to(i + 1);
-        return Percent;
-    }
-
-    if (match_word(i, "top")) {
-        // that's a top operator!
-        // (unput was forgorren already)
-        return Top;
-    }
-
-    if (match_word(i, "dup")) {
-        return Dup;
-    }
-
-    if (match_word(i, "swp")) {
-        return Swp;
-    }
-
-    if (match_word(i, "clr")) {
-        return Clr;
-    }
-
-    if (match_word(i, "sin")) {
-        return Sin;
-    }
-
-    if (match_word(i, "exp")) {
-        return Exp;
-    }
-
-    if (match_word(i, "pow")) {
-        return Pow;
-    }
-
-    if (c == '+') {
-        c = input_get(++i);
-        if (!isdigit(c)) {
-            // A plus followed by a non-digit => it's a plus operator!
-            forget_to(i);
-            return Plus;
-        }
-    } else if (c == '-') {
-        c = input_get(++i);
-        if (!isdigit(c)) {
-            // A minus followed by a non-digit => it's a minus operator!
-            forget_to(i);
-            return Minus;
-        }
-
-        // note that minus was encountered
-        sign = -1.0;
-    } else if (!isdigit(c)) {
-        // it's not a number, and not one of the operators
-        // meaning, it's unknown input
-        input_clear();
-        return Unknown;
-    }
-
-    // at this point, we should be parsing a number
-    oprd_put(parse_double(i) * sign);
-    return Operand;
+    // If all of the matches had failed - well, that's something weird
+    return Unknown;
 }
+
+#define MAX_LINE_LEN 100
 
 int main() {
     enum InputType type;
     double op1, op2, sin(double), exp(double), pow(double, double);
-    while (1) {
-        type = read_next();
-        // This was used for debug
-        /*
-        switch (type) {
-        case Operand:
-            printf("Operand: %f\n", oprd_pop());
-            break;
-        case Plus:
-            printf("Plus\n");
-            break;
-        case Minus:
-            printf("Minus\n");
-            break;
-        case Star:
-            printf("Star\n");
-            break;
-        case Slash:
-            printf("Slash\n");
-            break;
-        case Unknown:
-            printf("Unknown input received\n");
-            break;
-        }
-        */
-        switch (type) {
-        case Eof:
-            // break out of the cycle
-            goto end;
-        case Operand:
-            // do nothing. was already added to the stack
-            break;
-        case Plus:
-            // perform addition
-            op2 = oprd_pop();
-            op1 = oprd_pop();
-            oprd_put(op1 + op2);
-            break;
-        case Minus:
-            // perform subtraction
-            op2 = oprd_pop();
-            op1 = oprd_pop();
-            oprd_put(op1 - op2);
-            break;
-        case Star:
-            // perform multiplication
-            op2 = oprd_pop();
-            op1 = oprd_pop();
-            oprd_put(op1 * op2);
-            break;
-        case Slash:
-            // perform division
-            op2 = oprd_pop();
-            if (op2 == 0.0) {
-                // No allowed
-                fprintf(stderr,
-                        "\tAttempt to divide by number of my brain cells rn\n");
-                return -1;
-            }
-            op1 = oprd_pop();
-            oprd_put(op1 / op2);
-            break;
-        case Percent:
-            // perform modulo
-            op2 = oprd_pop();
-            if (op2 == 0.0) {
-                fprintf(stderr, "\tCan't take modulo by 0\n");
-                return -2;
-            }
-            op1 = oprd_pop();
-            oprd_put((double)((int)op1 % (int)op2));
-            break;
-        case Top:
-            // reaction depends on number of elements in the stack
-            switch (oprd_len()) {
-            case 0:
-                // there are no elements
-                // complete disaster
-                fprintf(stdout, "There are no operands in the stack\n");
+    char buf[MAX_LINE_LEN], *bufp;
+read:
+    while (fgets(buf, MAX_LINE_LEN, stdin) != NULL) {
+        bufp = buf;
+        for (;;) {
+            type = read_next(&bufp);
+            switch (type) {
+            case Unknown:
+                // print to stderr
+                fprintf(stderr, "\tGot unknown input: %s\n", bufp);
+            case Incomplete:
+                goto read;
+            case Operand:
+                // do nothing;
+                // operand was already added to the stack
                 break;
-            case 1:
-                // there's only one element
-                fprintf(stdout, "There's a single operand in the stack: %f\n",
-                        nth_oprd(0));
-                break;
-            default:
-                // there are at least 2 elements!
-                fprintf(stdout, "Last operands in the stack: [.., %f, %f]\n",
-                        nth_oprd(1), nth_oprd(0));
-                break;
-            }
-            break;
-        case Swp:
-            if (oprd_len() < 2)
-                fprintf(stdout, "Can't swp top operands: there are 0 or 1 "
-                                "operands in the stack\n");
-            else {
+            case Plus:
+                // perform addition
                 op2 = oprd_pop();
                 op1 = oprd_pop();
-                oprd_put(op2);
-                oprd_put(op1);
+                oprd_put(op1 + op2);
+                break;
+            case Minus:
+                // perform subtraction
+                op2 = oprd_pop();
+                op1 = oprd_pop();
+                oprd_put(op1 - op2);
+                break;
+            case Star:
+                // perform multiplication
+                op2 = oprd_pop();
+                op1 = oprd_pop();
+                oprd_put(op1 * op2);
+                break;
+            case Slash:
+                // perform division
+                op2 = oprd_pop();
+                if (op2 == 0.0) {
+                    // No allowed
+                    fprintf(
+                        stderr,
+                        "\tAttempt to divide by number of my brain cells rn\n");
+                    return -1;
+                }
+                op1 = oprd_pop();
+                oprd_put(op1 / op2);
+                break;
+            case Percent:
+                // perform modulo
+                op2 = oprd_pop();
+                if (op2 == 0.0) {
+                    fprintf(stderr, "\tCan't take modulo by 0\n");
+                    return -2;
+                }
+                op1 = oprd_pop();
+                oprd_put((double)((int)op1 % (int)op2));
+                break;
+            case Top:
+                // reaction depends on number of elements in the stack
+                switch (oprd_len()) {
+                case 0:
+                    // there are no elements
+                    // complete disaster
+                    fprintf(stdout, "There are no operands in the stack\n");
+                    break;
+                case 1:
+                    // there's only one element
+                    fprintf(stdout,
+                            "There's a single operand in the stack: %f\n",
+                            nth_oprd(0));
+                    break;
+                default:
+                    // there are at least 2 elements!
+                    fprintf(stdout,
+                            "Last operands in the stack: [.., %f, %f]\n",
+                            nth_oprd(1), nth_oprd(0));
+                    break;
+                }
+                break;
+            case Swp:
+                if (oprd_len() < 2)
+                    fprintf(stdout, "Can't swp top operands: there are 0 or 1 "
+                                    "operands in the stack\n");
+                else {
+                    op2 = oprd_pop();
+                    op1 = oprd_pop();
+                    oprd_put(op2);
+                    oprd_put(op1);
+                }
+                break;
+            case Dup:
+                if (oprd_len() < 1)
+                    fprintf(stdout, "Can't dup top operand, as there are no "
+                                    "operands in the stack");
+                else
+                    oprd_put(nth_oprd(0));
+                break;
+            case Sin:
+                oprd_put(sin(oprd_pop()));
+                break;
+            case Exp:
+                oprd_put(exp(oprd_pop()));
+                break;
+            case Pow:
+                op2 = oprd_pop();
+                op1 = oprd_pop();
+                if (op1 < 0.0) {
+                    fprintf(stderr, "\tPower with negative base is not "
+                                    "well-defined methmtically\n");
+                    return -3;
+                }
+                if (op1 == 0.0 && op2 <= 0.0) {
+                    fprintf(stderr, "\tPower of 0 with non-positive eponent is "
+                                    "not defined\n");
+                    return -4;
+                } else
+                    oprd_put(pow(op1, op2));
+                break;
+            case Clr:
+                oprd_clear();
+                break;
             }
-            break;
-        case Dup:
-            if (oprd_len() < 1)
-                fprintf(stdout, "Can't dup top operand, as there are no "
-                                "operands in the stack");
-            else
-                oprd_put(nth_oprd(0));
-            break;
-        case Sin:
-            oprd_put(sin(oprd_pop()));
-            break;
-        case Exp:
-            oprd_put(exp(oprd_pop()));
-            break;
-        case Pow:
-            op2 = oprd_pop();
-            op1 = oprd_pop();
-            if (op1 < 0.0) {
-                fprintf(stderr, "\tPower with negative base is not "
-                                "well-defined methmtically\n");
-                return -3;
-            }
-            if (op1 == 0.0 && op2 <= 0.0) {
-                fprintf(
-                    stderr,
-                    "\tPower of 0 with non-positive eponent is not defined\n");
-                return -4;
-            } else
-                oprd_put(pow(op1, op2));
-            break;
-        case Clr:
-            oprd_clear();
-            break;
-        case Unknown:
-            // print to stderr
-            fprintf(stderr, "\tGot unknown input!\n");
-            break;
         }
     }
-end:
-    // in the end, print to operand in the stack
+    // in the end, print top operand in the stack
     printf("Answer to everything is %f\n", oprd_pop());
 }
