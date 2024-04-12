26f3d16b91c329e85a98a17c3dfd5a5e9fa3245e
Dzuchun <Infernodambik@gmail.com>
2024-04-12 17:51:01 +0300

    Made 7-8 to use raw syscalls

--- task_7_8.c
+++ task_7_8.c
@@ -1,23 +1,49 @@
-
-
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <unistd.h>
 
-#define MAX_LINE_LENGTH 500
+#define REPORT(code, desc)                                                     \
+    if (code < 0) {                                                            \
+        fprintf(stderr, "%s, %s: %s", program_name, file_name, desc);          \
+        exit(-1);                                                              \
+    }
 
 int main(int argc, char **argv) {
-    char *program_name = *argv++, *file_name, line[MAX_LINE_LENGTH], *linep;
-    FILE *input_file;
+    char *program_name = *argv++, *file_name, buf[BUFSIZ];
+    int fd, out_fd = fileno(stdout);
+    long code;
     int page = 1;
     argc--;
     while (argc--) {
         file_name = *argv++;
-        input_file = fopen(file_name, "r");
-        printf("--- FILE: %s --- [ %d ] \n", file_name, page);
-        while (!feof(input_file) &&
-               (linep = fgets(line, MAX_LINE_LENGTH, input_file)) != NULL)
-            fputs(linep, stdout);
-        printf("--- [ %d ]\n\n", page);
-        fclose(input_file);
+        fd = (int)syscall(SYS_open, file_name, O_RDONLY);
+        REPORT(fd, "failed to open");
+
+        sprintf(buf, "\n--- FILE: %s --- [ %d ] \n", file_name, page);
+        code = syscall(SYS_write, out_fd, buf, strlen(buf));
+        REPORT(code, "failed to write page header");
+
+        for (;;) {
+            code = syscall(SYS_read, fd, buf, BUFSIZ);
+            REPORT(code, "failed to read");
+
+            if (code == 0)
+                // that means eof
+                break;
+
+            code = syscall(SYS_write, out_fd, buf, code);
+            REPORT(code, "failed to read");
+        }
+        sprintf(buf, "\n--- [ %d ]\n", page);
+        code = syscall(SYS_write, out_fd, buf, strlen(buf));
+        REPORT(code, "failed to write page footer");
+
+        code = syscall(SYS_close, fd);
+        REPORT(code, "failed to close");
+
         ++page;
     }
 }
